8dd7bc440941eb5aad4e88f69fbfda6ef00ec0eb
suhasini23 <ssumisingyan@gmail.com>
2020-07-26 13:46:40 +0530

    Updates

---
+++ precache-manifest.11cfb0e28919682e96115f7e18faae51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ef3a79edad6a4618e05383b0f3b66e",
+    "url": "/infilect/index.html"
+  },
+  {
+    "revision": "d34d6379dca27b12289d",
+    "url": "/infilect/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "29217be2a127d4302940",
+    "url": "/infilect/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d34d6379dca27b12289d",
+    "url": "/infilect/static/js/2.f671e7f0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/infilect/static/js/2.f671e7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29217be2a127d4302940",
+    "url": "/infilect/static/js/main.1a4c69ea.chunk.js"
+  },
+  {
+    "revision": "0549b72a5c1730400d74",
+    "url": "/infilect/static/js/runtime-main.1e2d7e23.js"
+  },
+  {
+    "revision": "ec93cd443f55ccf5802b8e3feaa29e89",
+    "url": "/infilect/static/media/aa.ec93cd44.jpg"
+  }
+]);
